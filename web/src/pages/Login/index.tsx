64be3a9e35445a35e49ef924d96a8daa190db8a6
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2024-09-22 13:07:24 -0300

    :tada: add notifications

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,9 +1,47 @@
-import { Link } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../hooks/useAuth";
 
 import { Container } from "./styles";
 import { StarrySky } from "../../components/StarrySky";
 
+import { emitterMessage } from "../../libs/toastify/emitterMessage";
+
 const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onChangeEmailValue = (e: FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onChangePasswordValue = (e: FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handleClick = async (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (email === "" || password === "")
+      return emitterMessage("Occured an error to register", "error");
+
+    try {
+      await login({ email, password });
+
+      emitterMessage("Login successfully", "success");
+
+      navigate("/home");
+    } catch (error) {
+      console.info(error);
+
+      return emitterMessage("Email/password incorrect!", "error");
+    }
+  };
+
   return (
     <div>
       <StarrySky />
@@ -14,10 +52,20 @@ const Login = () => {
             <h1>Log In</h1>
 
             <form>
-              <input type="text" placeholder="Email" required />
-              <input type="password" placeholder="Password" required />
+              <input 
+                type="text"
+                value={email}
+                placeholder="Email"
+                onChange={onChangeEmailValue}
+              />
+              <input 
+                type="password"
+                value={password} 
+                placeholder="Password"
+                onChange={onChangePasswordValue}
+              />
 
-              <button type="submit">Log In</button>
+              <button onClick={handleClick}>Log In</button>
 
               <div className="row">
                 <Link to="/sign-up">Sign Up</Link>
